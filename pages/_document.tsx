e971466ee0530be24f7b48bad63f85aa33c6ce8e
mohandast52 <mohandast52@gmail.com>
2026-01-12 15:00:40 +0530

    feat: add SEO meta tags and canonical link to improve search engine visibility

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,7 @@
 import { Head, Html, Main, NextScript } from 'next/document';
 
+import { SEO_CONFIG } from 'constants/seo';
+
 export default function Document() {
   return (
     <Html lang="en">
@@ -13,6 +15,30 @@ export default function Document() {
 
         {/* Favicon */}
         <link rel="icon" href="/favicon.ico" />
+
+        {/* Canonical (fallback) */}
+        <link rel="canonical" href={SEO_CONFIG.baseUrl} />
+
+        {/* Basic description (fallback) */}
+        <meta name="title" content={SEO_CONFIG.defaultTitle} />
+        <meta name="description" content={SEO_CONFIG.description} />
+
+        {/* Open Graph (fallback for crawlers that don't run JS) */}
+        <meta property="og:type" content={SEO_CONFIG.ogType} />
+        <meta property="og:site_name" content={SEO_CONFIG.defaultTitle} />
+        <meta property="og:title" content={SEO_CONFIG.defaultTitle} />
+        <meta property="og:description" content={SEO_CONFIG.description} />
+        <meta property="og:url" content={SEO_CONFIG.baseUrl} />
+        <meta property="og:image" content={`${SEO_CONFIG.baseUrl}${SEO_CONFIG.ogImage}`} />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="600" />
+
+        {/* Twitter (fallback) */}
+        <meta name="twitter:card" content={SEO_CONFIG.twitterCard} />
+        <meta name="twitter:site" content={SEO_CONFIG.twitterSite} />
+        <meta name="twitter:title" content={SEO_CONFIG.defaultTitle} />
+        <meta name="twitter:description" content={SEO_CONFIG.description} />
+        <meta name="twitter:image" content={`${SEO_CONFIG.baseUrl}${SEO_CONFIG.ogImage}`} />
 
         {/* Fonts */}
         <link
