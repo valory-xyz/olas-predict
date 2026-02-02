0278d4fa81d8771bf5eeb2afc83fe6a2685fe7e8
Rajat Verma <rajatverma5885045@gmail.com>
2026-02-02 23:26:21 +0530

    fix: import from constants

--- hooks/usePolystratBet.ts
+++ hooks/usePolystratBet.ts
@@ -2,6 +2,7 @@ import { useQuery } from '@tanstack/react-query';
 import { getPolymarketData } from 'graphql/queries';
 import { useMemo } from 'react';
 
+import { NA } from 'constants/index';
 import { TransformedPolymarketBet } from 'types/polymarket';
 
 const USDC_DECIMALS = 6;
@@ -25,13 +26,13 @@ export const usePolystratBet = (betId: string) => {
 
     const { question, amount, transactionHash } = bet;
     const outcomeIndex = parseInt(bet.outcomeIndex);
-    const position = question?.metadata?.outcomes?.[outcomeIndex] || 'n/a';
+    const position = question?.metadata?.outcomes?.[outcomeIndex] || NA;
 
     const betAmount = parseInt(amount) / Math.pow(10, USDC_DECIMALS);
     const amountWon = parseInt(marketParticipant.totalPayout) / Math.pow(10, USDC_DECIMALS);
 
     return {
-      question: question?.metadata?.title || 'n/a',
+      question: question?.metadata?.title || NA,
       position,
       transactionHash,
       betAmount,
